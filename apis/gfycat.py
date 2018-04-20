7ec57157198a1911222d49f7ef09ce6fc80d95a1
Tarinu <kaarel06@gmail.com>
2018-04-21 01:12:23 +0300

    Fixed not being able to connect to gfycat api

--- apis/gfycat.py
+++ apis/gfycat.py
@@ -13,19 +13,19 @@ class Gfycat(object):
         self.session = None  # type: aiohttp.ClientSession
         self.gfycats = None  # type: dict
 
-    async def before_request(self):
+    def before_request(self):
         if self.session is None:
             self.session = aiohttp.ClientSession()
 
     async def get_token(self):
-        await self.before_request()
-        async with self.session.post('https://api.gfycat.com/v1/oauth/token', data=json.dumps(self.config.get('token_data'))) as token:
+        self.before_request()
+        async with self.session.post('https://api.gfycat.com/v1/oauth/token', json=self.config.get('token_data')) as token:
             assert token.status == 200
             self.token = await token.json()
             self.header = {'Authorization': 'Bearer ' + self.token.get('access_token')}
 
     async def get_album_gfycats(self):
-        await self.before_request()
+        self.before_request()
         async with self.session.get('https://api.gfycat.com/v1/me/albums/'+self.config['album_id'],
                                     headers=self.header) as resp:
             result = await resp.json()
